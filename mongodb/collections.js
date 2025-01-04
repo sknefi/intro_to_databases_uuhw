8a0142cd6c70d696bac8840a71a7206aa1c9cf91
FK <filip.karika1@gmail.com>
2025-01-04 13:16:06 +0100

    mongodb - collection and queries UPDATED

--- mongodb/collections.js
+++ mongodb/collections.js
@@ -1,20 +1,40 @@
+// CREATE COLLECTIONS AND INSERT DOCUMENTS
+
+// These are just examples of ObjectId values
+const AUTHOR_ID = "31292038f911de352ed861e4a";
+const MEMBER_ID = "67790038f93dd352cd861b7c";
+const BOOK_ID = "a1797018f93dc352cdf61ee9";
+
 // Create collection author with one document
-db.author.insertOne(
-	{ _id: 1, name: "Robert", surname: "Kiyosaki", birthdate: new Date("1947-04-08") }
-);
+db.author.insertOne({
+  name: "Robert",
+  surname: "Kiyosaki",
+  birthdate: new Date("1947-04-08"),
+  books: [] // This field will be populated with the ObjectId of the book document
+});
 
 // Create collection book with one document
-db.book.insertOne(
-	{ _id: 1, author_id: 1, title: "Rich Dad Poor Dad", published: new Date("1997-04-01") }
-);
+db.book.insertOne({
+  author_id: AUTHOR_ID,
+  title: "Rich Dad Poor Dad",
+  published: new Date("1997-04-01"),
+});
 
 // Create collection member with one document
-db.member.insertOne(
-	{ _id: 1, name: "Palo", surname: "Habera" }
-);
+db.member.insertOne({
+  name: "Palo",
+  surname: "Habera",
+  contact: {
+    phone: "123-456-7890",
+    email: "palo.habera@example.com",
+    address: "123 Random Street, Random City, 12345",
+  },
+});
 
 // Create collection loan with one document
-db.loan.insertOne(
-	{  _id: 1, member_id: 1, book_id: 1, loan_date: new Date("2023-01-15"), return_date: new Date("2023-02-15") }
-);
-  
+db.loan.insertOne({
+  member_id: AUTHOR_ID,
+  book_id: BOOK_ID,
+  loan_date: new Date("2023-01-15"),
+  return_date: new Date("2023-02-15"),
+});
--- mongodb/queries.js
+++ mongodb/queries.js
@@ -1,83 +1,85 @@
+// MAIN SUBJECT: Library Database
+
 // Projection and selection of members who have taken a loan
-db.loan.aggregate([
-	{
-	$lookup: {
-		from: "member",
-		localField: "member_id",
-		foreignField: "_id",
-		as: "member_details"
-	  }
-	},
-	{
-	$project: {
-		"member_details.name": 1,
-		"member_details.surname": 1
-	  }
-	},
-	{ $unwind: "$member_details" },
-	{ $group: { _id: "$member_details" } }
+db.member.aggregate([
+  {
+    $lookup: {
+      from: "loan",
+      localField: "_id",
+      foreignField: "member_id",
+      as: "loans",
+    },
+  },
+  { $match: { loans: { $ne: [] } } },
+  { $project: { name: 1, surname: 1 } },
 ]);
-  
+
 // Selection criteria for books borrowed in 2023
 db.loan.aggregate([
-	{
-	$lookup: {
-		from: "book",
-		localField: "book_id",
-		foreignField: "_id",
-		as: "book_details"
-	  }
-	},
-	{
-	$match: {
-		loan_date: { $gte: new Date("2023-01-01"), $lte: new Date("2023-12-31") }
-	  }
-	},
-	{
-	$project: {
-		"book_details.title": 1,
-		loan_date: 1,
-		return_date: 1
-	  }
-	}
+  {
+    $lookup: {
+      from: "book",
+      localField: "book_id",
+      foreignField: "_id",
+      as: "book_info",
+    },
+  },
+  { $unwind: "$book_info" },
+  {
+    $match: {
+      loan_date: { $gte: new Date("2023-01-01"), $lte: new Date("2023-12-31") },
+    },
+  },
+  { $project: { title: "$book_info.title", loan_date: 1, return_date: 1 } },
 ]);
 
 // Subquery for books written by Kiyosaki
-db.book.find({
-	author_id: db.author.findOne({ surname: "Kiyosaki" })._id
-});
+db.book.aggregate([
+  {
+    $lookup: {
+      from: "author",
+      localField: "author_id",
+      foreignField: "_id",
+      as: "author_info",
+    },
+  },
+  { $unwind: "$author_info" },
+  { $match: { "author_info.surname": "Kiyosaki" } },
+  { $project: { title: 1 } },
+]);
 
 // Subquery for members who have not borrowed a book
-db.member.find({
-	_id: { $nin: db.loan.distinct("member_id") }
-});
+db.member.aggregate([
+  {
+    $lookup: {
+      from: "loan",
+      localField: "_id",
+      foreignField: "member_id",
+      as: "loans",
+    },
+  },
+  { $match: { loans: { $size: 0 } } },
+  { $project: { name: 1, surname: 1 } },
+]);
 
 // Aggregation and sorting of members who have borrowed the most books
 db.loan.aggregate([
-	{
-	$lookup: {
-		from: "member",
-		localField: "member_id",
-		foreignField: "_id",
-		as: "member_details"
-	  }
-	},
-	{
-	$group: {
-		_id: "$member_id",
-		loan_count: { $sum: 1 },
-		member_name: { $first: "$member_details.name" },
-		member_surname: { $first: "$member_details.surname" }
-	  }
-	},
-	{ $sort: { loan_count: -1 } },
-	{
-	$project: {
-		member_name: 1,
-		member_surname: 1,
-		loan_count: 1
-	  }
-	}
+  {
+    $lookup: {
+      from: "member",
+      localField: "member_id",
+      foreignField: "_id",
+      as: "member_info",
+    },
+  },
+  { $unwind: "$member_info" },
+  {
+    $group: {
+      _id: { name: "$member_info.name", surname: "$member_info.surname" },
+      loan_count: { $sum: 1 },
+    },
+  },
+  { $sort: { loan_count: -1 } },
 ]);
 
 // SOME BONUSES
@@ -90,36 +92,33 @@ db.book.createIndex({ title: 1, author_id: 1 }, { unique: true });
 
 // Active loans view
 db.loan.aggregate([
-	{
-	$lookup: {
-		from: "member",
-		localField: "member_id",
-		foreignField: "_id",
-		as: "member_details"
-	  }
-	},
-	{
-	$lookup: {
-		from: "book",
-		localField: "book_id",
-		foreignField: "_id",
-		as: "book_details"
-	  }
-	},
-	{
-	$match: {
-		return_date: { $gt: new Date() }
-	  }
-	},
-	{
-	$project: {
-		loan_id: 1,
-		"member_details.name": 1,
-		"member_details.surname": 1,
-		"book_details.title": 1,
-		loan_date: 1,
-		return_date: 1
-	  }
-	}
-  ]);
-  
+  {
+    $lookup: {
+      from: "member",
+      localField: "member_id",
+      foreignField: "_id",
+      as: "member_info",
+    },
+  },
+  { $unwind: "$member_info" },
+  {
+    $lookup: {
+      from: "book",
+      localField: "book_id",
+      foreignField: "_id",
+      as: "book_info",
+    },
+  },
+  { $unwind: "$book_info" },
+  { $match: { return_date: { $gt: new Date() } } },
+  {
+    $project: {
+      loan_id: 1,
+      member_name: "$member_info.name",
+      member_surname: "$member_info.surname",
+      book_title: "$book_info.title",
+      loan_date: 1,
+      return_date: 1,
+    },
+  },
+]);
